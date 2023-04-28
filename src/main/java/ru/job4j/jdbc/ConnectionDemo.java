51c32bbb3267af161ca36a819e3f0095c3e12141
KirAlex008 <kirshaav@yandex.ru>
2023-04-28 18:06:36 -0400

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -7,10 +7,20 @@ import java.sql.SQLException;
 import ru.job4j.io.Config;
 
 public class ConnectionDemo {
+
+    private static Connection getConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        return DriverManager.getConnection(url, login, password);
+    }
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         Config config = new Config("src/main/resources/app.properties");
         config.load();
+
         try (Connection connection = DriverManager.getConnection(
                 config.value("url"),
                 config.value("login"),
